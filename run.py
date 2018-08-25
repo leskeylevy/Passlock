5ca3093a446d34f622d5f64d8a55f4010f67c35c
Levy <leskeylevy@gmail.com>
2018-08-25 18:45:18 +0300

    creating and initiating run.py

---
+++ run.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3.6
+from user import User
+
+
+def create_user(fname, lname, email, location):
+    '''
+    function to create a new user
+    '''
+    new_user = User(fname, lname, email, location)
+    return new_user
