db29b4e6e27983774d9f0d78432d90ca95f4cc86
Levy <leskeylevy@gmail.com>
2018-08-25 18:55:01 +0300

    added save function in run.py

--- run.py
+++ run.py
@@ -8,3 +8,8 @@ def create_user(fname, lname, email, location):
     '''
     new_user = User(fname, lname, email, location)
     return new_user
+def save_users(user):
+    '''
+    function to save user
+    '''
+    user.save_user()
