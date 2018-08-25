b15cfb414ccfd1f3b04ae01cf852dc3dd252635c
Levy <leskeylevy@gmail.com>
2018-08-25 16:54:30 +0300

    test to see if the application can store/save multiple users

--- user_test.py
+++ user_test.py
@@ -16,13 +16,27 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.email, 'leskeylevy@gmail.com')
         self.assertEqual(self.new_user.location, 'Nairobi')
 
+    def tearDown(self):
+        User.user_list = []
+
 
     def test_save_user(self):
         '''
         test to see if User object is saved into the user list
         '''
         self.new_user.save_user()
-        self.assertEqual(len(User.user_list),1)
+        self.assertEqual(len(User.user_list) , 1)
+
+
+    def test_save_multiple_user(self):
+
+        '''
+        test save multiple users to checkif we can save multiple users
+        '''
+        self.new_user.save_user()
+        test_user = User('leskey', 'levy', 'leskeylevy@gmail.com', 'Nairobi')
+        test_user.save_user()
+        self.assertEqual(len(User.user_list), 2)
 
 
 if __name__ == '__main__':
