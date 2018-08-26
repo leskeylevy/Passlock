5c48b19524555d87575279e1d6b80c1f919d2575
Levy <leskeylevy@gmail.com>
2018-08-26 15:06:28 +0300

    first credentials test

--- credential_test.py
+++ credential_test.py
@@ -2,6 +2,24 @@ import unittest
 from credential import Credential
 
 class TestCredential(unittest.TestCase):
-    '''
+    """
     test class defining test cases for credentials behaviour
-    '''
+    """
+    def setUp(self):
+        '''
+        setup method to run b4 each test
+        '''
+        self.new_credential = Credential('Facebook','leskeylevy','mysecretpassword','leskeylevy@gmail.com')
+
+    def test_init(self):
+        '''
+        test to see if the class was initiated properly
+        '''
+        self.assertEqual(self.new_credential.site_name,"Facebook")
+        self.assertEqual(self.new_credential.userName, "leskeylevy")
+        self.assertEqual(self.new_credential.password, "mysecretpassword")
+        self.assertEqual(self.new_credential.emailUsed, "leskeylevy@gmail.com")
+
+
+if __name__=='__main__':
+    unittest.main()
